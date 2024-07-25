8f91dd423c7dfbdfaa5772926663e507f3c7fd6f
Syra Dominguez <syradominguez.dev@gmail.com>
2024-07-25 17:04:13 +0200

    Añadida OffersList con estructura basica + module.css

---
+++ src/pages/offers/OffersList.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout/Layout';
+// import styles from "./OffersList.module.css";
+
+export function OffersList() {
+  return (
+    <>
+      <Layout title="Offers List" page="offerslist"></Layout>
+    </>
+  );
+}
