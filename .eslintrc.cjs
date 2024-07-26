0f5285165bc2dd93f19d003c57e4fd35a4574797
Syra Dominguez <syradominguez.dev@gmail.com>
2024-07-26 10:03:42 +0200

    chore: Update ESLint configuration to disable prop-types validation and add @typescript-eslint/no-unused-vars rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,4 +28,16 @@ module.exports = {
     ],
     'react/prop-types': 'off', // Add this line to disable prop-types validation
   },
+  "@typescript-eslint/no-unused-vars": [
+    "error",
+    {
+      "args": "all",
+      "argsIgnorePattern": "^_",
+      "caughtErrors": "all",
+      "caughtErrorsIgnorePattern": "^_",
+      "destructuredArrayIgnorePattern": "^_",
+      "varsIgnorePattern": "^_",
+      "ignoreRestSiblings": true
+    }
+  ]
 };
