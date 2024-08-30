60019f2c5fe1e3302299a9f885d14ac4bfb91cf8
Carles Espuñes <cespuess@gmail.com>
2024-08-30 17:05:59 +0200

    internalitation and styles ApplicantInfo.tsx

--- src/components/dashboard/ApplicantInfo.tsx
+++ src/components/dashboard/ApplicantInfo.tsx
@@ -6,11 +6,16 @@ import { useSelector } from 'react-redux';
 import { getApplicantInfo, getUi } from '../../store/selectors';
 import { uiSlice } from '../../store/reducers/uiSlice';
 import Notification from '../common/Notification';
+import { useTranslation } from 'react-i18next';
+import { Button } from '../common/Button';
+import { Link } from 'react-router-dom';
+import styles from './ApplicantInfo.module.css';
 
 export default function ApplicantInfo() {
   const dispatch = useDispatch<AppDispatch>();
   const applicant = useSelector(getApplicantInfo);
   const { error } = useSelector(getUi);
+  const { t } = useTranslation();
 
   useEffect(() => {
     dispatch(getInfoApplicantAction());
@@ -18,14 +23,37 @@ export default function ApplicantInfo() {
 
   function showInfo() {
     return (
-      <div>
-        <img src="" alt={applicant.photo} />
-        <h3>Nombre: {`${applicant.name} ${applicant.lastName}`}</h3>
-        <p>Email: {applicant.email}</p>
-        <p>Teléfono: {applicant.phone}</p>
-        <p>Ciudad: {applicant.ubication} </p>
-        <p>Modalidad: {`${applicant.typeJob} & ${applicant.internType}`}</p>
-      </div>
+      <>
+        <div className={styles.container}>
+          <div className={styles.profile__photo}>
+            <img
+              src="https://previews.123rf.com/images/yupiramos/yupiramos1609/yupiramos160911129/62789324-avatar-cara-hombre-dibujos-animados-hombre-persona-ilustraci%C3%B3n-vectorial.jpg"
+              alt={applicant.photo}
+            />
+          </div>
+          <h3>{`${applicant.name} ${applicant.lastName}`}</h3>
+          <div className={styles.profile__info}>
+            <p>
+              <span>{t('fields.email')}:</span> {applicant.email}
+            </p>
+            <p>
+              <span>{t('fields.phone')}:</span> {applicant.phone}
+            </p>
+            <p>
+              <span>{t('fields.location')}:</span> {applicant.ubication}
+            </p>
+            <p>
+              <span>{t('forms.internship_type')}:</span>{' '}
+              {`${applicant.typeJob} & ${applicant.internType}`}
+            </p>
+          </div>
+          <div className={styles.button}>
+            <Link to="/user/edit">
+              <Button>{t('titles.userprofile_edit')}</Button>
+            </Link>
+          </div>
+        </div>
+      </>
     );
   }
 
