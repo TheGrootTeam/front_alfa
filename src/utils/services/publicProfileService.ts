9a9294aff48c9f19c4cf8be40619d6c5bf7cca34
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-08-30 23:05:48 +0200

    bug in import

--- src/utils/services/publicProfileService.ts
+++ src/utils/services/publicProfileService.ts
@@ -1,11 +1,11 @@
 import { client } from '../../api/client';
-import { infocompanyMapped } from '../utilsInfoCompany';
+import { infoCompanyMapped } from '../utilsInfoCompany';
 
 export async function getPublicInfo(id:string | undefined, type:string) {
   //Falta crear interface y cambiar any per la interface
   const publicInfo: any = await client.get(
     `/profile/${type}/${id}`
   );
-  const mappedInfo = infocompanyMapped(publicInfo);
+  const mappedInfo = infoCompanyMapped(publicInfo);
   return mappedInfo;
 }
