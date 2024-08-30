129057dba2700a361d0c7dd2227ff230a0b15eb0
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-08-30 19:07:20 +0200

    publicProfile

--- src/utils/services/publicProfileService.ts
+++ src/utils/services/publicProfileService.ts
@@ -1,7 +1,7 @@
 import { client } from '../../api/client';
 import { infocompanyMapped } from '../utilsInfoCompany';
 
-export async function getPublicInfo(id:string, type:string) {
+export async function getPublicInfo(id:string | undefined, type:string) {
   //Falta crear interface y cambiar any per la interface
   const publicInfo: any = await client.get(
     `/profile/${type}/${id}`
