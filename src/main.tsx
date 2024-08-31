f2e48c09927878156e2705c4b94be6aa2eb7c6cc
Carles Espuñes <cespuess@gmail.com>
2024-08-31 11:38:44 +0200

    problems with building solved

--- src/main.tsx
+++ src/main.tsx
@@ -8,22 +8,14 @@ import './i18n';
 import { store } from './store/store';
 import { router } from './router';
 import storage from './utils/storage';
-import { authRememberSession } from './utils/utilsAuth';
-import { initialAuth } from './store/reducers/authSlice';
-import { removeAuthorizationHeader } from './api/client';
+import { authToken } from './utils/utilsAuth';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 
 const token: string | null = storage.get('key');
 
 if (token) {
-  try {
-    const isCompany = await authRememberSession(token);
-    store.dispatch(initialAuth({ isCompany }));
-  } catch (error) {
-    removeAuthorizationHeader();
-    storage.remove('key');
-  }
+  authToken(token);
 }
 
 root.render(
--- src/utils/utilsAuth.ts
+++ src/utils/utilsAuth.ts
@@ -1,8 +1,24 @@
-import { setAuthorizationHeader } from '../api/client';
+import {
+  removeAuthorizationHeader,
+  setAuthorizationHeader,
+} from '../api/client';
+import { initialAuth } from '../store/reducers/authSlice';
+import { store } from '../store/store';
 import { authVerify } from './services/authService';
+import storage from './storage';
 
 export const authRememberSession = async (token: string) => {
   setAuthorizationHeader(token);
   const isCompany = await authVerify();
   return isCompany.isCompany;
+};
+
+export const authToken = async (token: string) => {
+  try {
+    const isCompany = await authRememberSession(token);
+    store.dispatch(initialAuth({ isCompany }));
+  } catch (error) {
+    removeAuthorizationHeader();
+    storage.remove('key');
+  }
 };
