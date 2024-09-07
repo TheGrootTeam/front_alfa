991b98984c7b5e033feef9433ddc42176a4eccea
Carles Espuñes <cespuess@gmail.com>
2024-09-07 16:33:49 +0200

    change-password method PATCH and style changePasswordPage

--- src/pages/password/ChangePasswordPage.tsx
+++ src/pages/password/ChangePasswordPage.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import Layout from '../../components/layout/Layout';
 import { ChangePasswordForm } from '../../components/forms/ChangePasswordForm';
-// import styles from "./ChangePasswordPage.module.css";
+import styles from './ChangePasswordPage.module.css';
 
 export function ChangePasswordPage() {
   const { t } = useTranslation();
   return (
     <>
-      <Layout title={t('buttons.change_password')} page="changePassword">
+      <Layout
+        title={t('buttons.change_password')}
+        page="changePassword"
+        mainClassName={styles.change_password_page}
+      >
         <ChangePasswordForm />
       </Layout>
     </>
--- src/utils/services/passwordService.ts
+++ src/utils/services/passwordService.ts
@@ -2,5 +2,5 @@ import { client } from '../../api/client';
 import { IDataApiChangePassword } from '../interfaces/IFormElements';
 
 export async function changePasswordService(data: IDataApiChangePassword) {
-  await client.post('/changePassword', data);
+  await client.patch('/changePassword', data);
 }
