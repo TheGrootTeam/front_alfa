cd45573265826f24666e1bfef78478e9324d3242
Marta <vilaseca.marta@gmail.com>
2024-09-18 14:25:46 +0200

    Edited CSS styles for the public Applicant profile

--- src/pages/applicants/ProfileApplicant.tsx
+++ src/pages/applicants/ProfileApplicant.tsx
@@ -67,8 +67,12 @@ export function UserProfilePage() {
     dispatch(uiSlice.actions.resetError());
   };
 
-  const photoSRC = `${import.meta.env.VITE_FILE_PATH}/photo/${photo}` ||`${import.meta.env.VITE_DEFAULT_FILE_URL}`;
-  const cvSRC = `${import.meta.env.VITE_FILE_PATH}/cv/${cv}` ||`${import.meta.env.VITE_DEFAULT_FILE_URL}`;
+  const photoSRC =
+    `${import.meta.env.VITE_FILE_PATH}/photo/${photo}` ||
+    `${import.meta.env.VITE_DEFAULT_FILE_URL}`;
+  const cvSRC =
+    `${import.meta.env.VITE_FILE_PATH}/cv/${cv}` ||
+    `${import.meta.env.VITE_DEFAULT_FILE_URL}`;
 
   return (
     <Layout title={t('titles.userprofile')} page="userprofile">
@@ -76,73 +80,86 @@ export function UserProfilePage() {
       {error && (
         <Notification type="error" message={error} onClick={resetError} />
       )}
-      <header className={styles.header}>
-        <div className={styles.profile__photo}>
-          <img src={photoSRC} alt="" />
-        </div>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <div className={styles.profile__photo}>
+            <img src={photoSRC} alt="" />
+          </div>
 
-        <div className={styles.profile__data}>
-          <h2>
-            {name} {lastName}
-          </h2>
-          <h3>{wantedRol.map((item) => item.rol).join(', ')}</h3>
-          <p className={styles.profile__location}>
-            <span className={`material-symbols-outlined ${styles.iconSmall}`}>
-              location_on
-            </span>
-            {ubication}
-          </p>
-          <a className={styles.profile__downloadCV} href={cvSRC}>
-            {t('forms.cv_dl_button')}
-          </a>
-        </div>
+          <div className={styles.profile__data}>
+            <h2>
+              {name} {lastName}
+            </h2>
+            <h3>{wantedRol.map((item) => item.rol).join(', ')}</h3>
+            <p className={styles.profile__location}>
+              <span className={`material-symbols-outlined ${styles.iconSmall}`}>
+                location_on
+              </span>
+              {ubication}
+            </p>
+            <a className={styles.profile__downloadCV} href={cvSRC}>
+              <span className={`material-symbols-outlined ${styles.iconSmall}`}>
+                download
+              </span>
+              {t('forms.cv_dl_button')}
+            </a>
+          </div>
 
-        {/* edit button if user */}
-      </header>
+          {/* edit button if user */}
+        </header>
 
-      <section className={styles.profile__details}>
-        <div className={styles.profile__skills}>
-          <h3>
-            <span className={`material-symbols-outlined ${styles.iconh3}`}>
-              manufacturing
-            </span>
-            {t('titles.main_skills')}
-          </h3>
-          <ul>
-            {skills.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
-        </div>
+        <section className={styles.profile__details}>
+          <div className={styles.profile__skills}>
+            <h3>
+              <span className={`material-symbols-outlined ${styles.iconh3}`}>
+                manufacturing
+              </span>
+              {t('titles.main_skills')}
+            </h3>
+            <ul>
+              {skills.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+            </ul>
+          </div>
 
-        <div className={styles.profile__preferences}>
-          <h3>
-            <span className={`material-symbols-outlined ${styles.iconh3}`}>
-              work
-            </span>
-            {t('titles.internship_preferences')}
-          </h3>
-          <dl>
-            <dt>{t('forms.job_type')}</dt> <dd>{jobType}</dd>
-            <dt>{t('forms.internship_type')}</dt> <dd>{internType}</dd>
-          </dl>
-        </div>
+          <div className={styles.profile__preferences}>
+            <h3>
+              <span className={`material-symbols-outlined ${styles.iconh3}`}>
+                work
+              </span>
+              {t('titles.internship_preferences')}
+            </h3>
+            <ul>
+              <li>
+                <strong>{t('forms.job_type')}</strong> {jobType}
+              </li>
+              <li>
+                <strong>{t('forms.internship_type')}</strong> {internType}
+              </li>
+            </ul>
+          </div>
 
-        <div className={styles.profile__availability}>
-          <h3>
-            <span className={`material-symbols-outlined ${styles.iconh3}`}>
-              check_circle
-            </span>
-            {t('titles.availability')}
-          </h3>
-          <dl>
-            <dt>{t('forms.willing_to_relocate')}</dt>
-            <dd>{mobility ? t('gen.yes') : t('gen.no')}</dd>
-            <dt>{t('forms.available_immediately')}</dt>
-            <dd>{disponibility ? t('gen.yes') : t('gen.no')}</dd>
-          </dl>
-        </div>
-      </section>
+          <div className={styles.profile__availability}>
+            <h3>
+              <span className={`material-symbols-outlined ${styles.iconh3}`}>
+                check_circle
+              </span>
+              {t('titles.availability')}
+            </h3>
+            <ul>
+              <li>
+                <strong>{t('forms.willing_to_relocate')}</strong>
+                {mobility ? t('gen.yes') : t('gen.no')}
+              </li>
+              <li>
+                <strong>{t('forms.available_immediately')}</strong>
+                {disponibility ? t('gen.yes') : t('gen.no')}
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 }
